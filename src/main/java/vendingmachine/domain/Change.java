fb1033eeaa342844ff61c0c775ecf3db8778df57
yoonsung.jung <ybook2012@gmail.com>
2021-05-31 16:54:26 +0900

    feat(Change) : 잔돈을 차감하는 기능 구현

--- src/main/java/vendingmachine/domain/Change.java
+++ src/main/java/vendingmachine/domain/Change.java
@@ -1,5 +1,7 @@
 package vendingmachine.domain;
 
+import java.util.Objects;
+
 public class Change {
     public static final int AMOUNT_LOW_LIMIT = 0;
     private final int amount;
@@ -17,5 +19,22 @@ public class Change {
 
     public int getAmount() {
         return amount;
+    }
+
+    public Change reduce(final int deductAmount) {
+        return new Change(amount - deductAmount);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Change change = (Change) o;
+        return amount == change.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
     }
 }
--- src/test/java/vendingmachine/domain/ChangeTest.java
+++ src/test/java/vendingmachine/domain/ChangeTest.java
@@ -1,5 +1,6 @@
 package vendingmachine.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -8,6 +9,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ChangeTest {
+
+    private final static int DEFAULT_VALUE = 500;
+    private Change change;
+
+    @BeforeEach
+    void setUp() {
+        change = new Change(DEFAULT_VALUE);
+    }
 
     @DisplayName("잘못된 값으로 잔돈을 만드려고 하면, 예외가 발생한다.")
     @ParameterizedTest
@@ -21,8 +30,15 @@ public class ChangeTest {
     @DisplayName("잔돈의 금액을 확인하면, 올바른 금액이 나온다.")
     @ParameterizedTest
     @ValueSource(ints = {100, 200, 300, 400})
-    void changeGetAmountTest(int amount) {
+    void changeGetAmountTest(final int amount) {
         Change change = new Change(amount);
         assertThat(change.getAmount()).isEqualTo(amount);
+    }
+
+    @DisplayName("잔돈의 금액을 차감하면, 남은 금액이 차감된다.")
+    @ParameterizedTest
+    @ValueSource(ints = {10, 49, 50, 111})
+    void deductChange(final int deductAmount) {
+        assertThat(change.reduce(deductAmount)).isEqualTo(new Change(DEFAULT_VALUE - deductAmount));
     }
 }
