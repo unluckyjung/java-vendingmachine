ad24618c6cab83d13fefa268d210c19dc5e87119
Seed <hsik0225@gmail.com>
2021-06-01 21:30:50 +0900

    feat : 금액에 맞는 동전을 찾는 기능 구현

--- src/main/java/vendingmachine/model/Coin.java
+++ src/main/java/vendingmachine/model/Coin.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 public enum Coin {
     FIVE_HUNDRED(500), ONE_HUNDRED(100), FIFTY(50), TEN(10);
 
+    public static final String BLANK = " ";
+
     private final int value;
 
     Coin(int value) {
@@ -17,6 +19,19 @@ public enum Coin {
         return Arrays.stream(Coin.values())
                      .sorted((c1, c2) -> c2.value - c1.value)
                      .collect(Collectors.toList());
+    }
+
+    public static List<Coin> toCoins(String money) {
+        return Arrays.stream(money.split(BLANK))
+                     .map(Coin::findByValue)
+                     .collect(Collectors.toList());
+    }
+
+    private static Coin findByValue(String money) {
+        return Arrays.stream(Coin.values())
+                     .filter(coin -> coin.value == Integer.parseInt(money))
+                     .findAny()
+                     .orElseThrow(() -> new IllegalArgumentException("없는 동전입니다."));
     }
 
     public int getValue() {
