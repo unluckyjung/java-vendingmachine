3be2e85d74c0543d411a31d3c1294ba657038a41
Seed <hsik0225@gmail.com>
2021-06-01 21:32:53 +0900

    feat : OutputView 기능 구현

---
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -0,0 +1,33 @@
+package vendingmachine.view;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import vendingmachine.model.Coin;
+import vendingmachine.model.Product;
+import vendingmachine.model.Products;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+public class OutputView {
+
+    public static void printPurchasedProducts(Products purchasedProducts) {
+        Map<String, Long> purchaseProductMap = purchasedProducts.getProducts()
+                                                                .stream()
+                                                                .collect(groupingBy(Product::getName, counting()));
+
+        List<String> products = new ArrayList<>();
+        for (Map.Entry<String, Long> entry : purchaseProductMap.entrySet()) {
+            products.add(String.format("%s %d개", entry.getKey(), entry.getValue()));
+        }
+
+        System.out.printf("구입한 상품들은 %s 입니다.", String.join(", ", products));
+
+    }
+
+    public static void printChanges(List<Coin> changes) {
+        System.out.printf("남은 잔돈은 %s 입니다.", changes);
+    }
+}
