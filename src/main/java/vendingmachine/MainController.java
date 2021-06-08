bb9a733f16ce0309aafb5a514e4fa5bd3fb8f08f
yoonsung.jung <ybook2012@gmail.com>
2021-06-08 19:34:23 +0900

    fix : 스택 오버플로우 해결

--- src/main/java/vendingmachine/MainController.java
+++ src/main/java/vendingmachine/MainController.java
@@ -1,11 +1,11 @@
 package vendingmachine;
 
-import java.util.Arrays;
-import java.util.function.Consumer;
-
 import vendingmachine.model.VendingMachine;
 import vendingmachine.view.InputView;
 import vendingmachine.view.OutputView;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
 
 public enum MainController {
     ADMINISTRATOR_MODE(1, AdministratorController::control),
@@ -34,11 +34,11 @@ public enum MainController {
 
     private static MainController findFunction(int functionNumber) {
         return Arrays.stream(MainController.values())
-                     .filter(function -> function.functionNumber == functionNumber)
-                     .findAny()
-                     .orElseGet(() -> {
-                         System.out.println("없는 메뉴 번호입니다. 다시 선택해주세요!");
-                         return MainController.findFunction(functionNumber);
-                     });
+                .filter(function -> function.functionNumber == functionNumber)
+                .findAny()
+                .orElseGet(() -> {
+                    System.out.println("없는 메뉴 번호입니다. 다시 선택해주세요!");
+                    return MainController.findFunction(InputView.inputFromMainView());
+                });
     }
 }
