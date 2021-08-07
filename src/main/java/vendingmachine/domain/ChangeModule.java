83ae12be7d052caa0bcd50de28be05ff28e04656
unluckyjung <ybook2012@gmail.com>
2021-08-08 00:00:51 +0900

    feat(ChangeModule): 보유하고 있는 돈을 증감시키는 기능

---
+++ src/main/java/vendingmachine/domain/ChangeModule.java
@@ -0,0 +1,18 @@
+package vendingmachine.domain;
+
+public class ChangeModule {
+
+    private Money change;
+
+    public ChangeModule(final Money money) {
+        this.change = money;
+    }
+
+    public void reduce(final int amount) {
+        change = change.reduce(amount);
+    }
+
+    public void increase(final Money money) {
+        change = change.increase(money.getAmount());
+    }
+}
