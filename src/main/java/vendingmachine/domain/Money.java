38d91ad2d75db2cf566dd74d33051d54d1429b7f
unluckyjung <ybook2012@gmail.com>
2021-08-07 23:50:38 +0900

    feat: Money 도메인 구현

---
+++ src/main/java/vendingmachine/domain/Money.java
@@ -0,0 +1,30 @@
+package vendingmachine.domain;
+
+public class Money {
+
+    private static final int AMOUNT_LOW_LIMIT = 0;
+    private final int amount;
+
+    public Money(final int amount) {
+        validateAmount(amount);
+        this.amount = amount;
+    }
+
+    private void validateAmount(final int amount) {
+        if (amount < AMOUNT_LOW_LIMIT) {
+            throw new IllegalArgumentException("돈은 음수가 될 수 없습니다");
+        }
+    }
+
+    public Money reduce(final int deductAmount) {
+        return new Money(amount - deductAmount);
+    }
+
+    public Money increase(final int increaseAmount) {
+        return new Money(amount + increaseAmount);
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
